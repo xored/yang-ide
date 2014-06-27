29dee93bd4a3b12027a7f0141558e791ac549d47
Andrey Sobolev <andrey.sobolev@xored.com>
2014-06-27 21:50:49 +0700

    * fix comment highlighting.

--- plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/YangPartitionScanner.java
+++ plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/YangPartitionScanner.java
@@ -5,30 +5,29 @@ import java.util.List;
 
 import org.eclipse.jface.text.rules.*;
 
-
 /**
  * @author Alexey Kholupko
- *
  */
 public class YangPartitionScanner extends RuleBasedPartitionScanner {
 
-	public final static String YANG_STRING = "__yang_string";
-	public final static String YANG_COMMENT = "__yang_comment";
+    public final static String YANG_STRING = "__yang_string";
+    public final static String YANG_COMMENT = "__yang_comment";
 
-	public YangPartitionScanner() {
+    public YangPartitionScanner() {
 
-	    List<IPredicateRule> rules= new ArrayList<IPredicateRule>();
-	    
-        IToken multiLinestring = new Token(YANG_STRING);       
-        //TODO escaping " or ' in each other sequence 
+        List<IPredicateRule> rules = new ArrayList<IPredicateRule>();
+
+        IToken comment = new Token(YANG_COMMENT);
+        rules.add(new MultiLineRule("/*", "*/", comment));
+        rules.add(new EndOfLineRule("//", comment));
+
+        IToken multiLinestring = new Token(YANG_STRING);
+        // TODO escaping " or ' in each other sequence
         rules.add(new MultiLineRule("\"", "\"", multiLinestring, '\\'));
         rules.add(new MultiLineRule("'", "'", multiLinestring, '\\'));
-        
-        IToken comment = new Token(YANG_COMMENT);   
-        rules.add(new MultiLineRule("/*", "*/", comment));
-		
-        IPredicateRule[] result= new IPredicateRule[rules.size()];
+
+        IPredicateRule[] result = new IPredicateRule[rules.size()];
         rules.toArray(result);
         setPredicateRules(result);
-	}
+    }
 }
