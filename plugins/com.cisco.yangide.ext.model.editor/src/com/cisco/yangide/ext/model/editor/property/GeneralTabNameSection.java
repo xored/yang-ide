556491fe6272a92e134f8a6af678a1f9f3ed5213
Anna No <anna.no.86@gmail.com>
2014-08-26 10:55:38 +0700

    remove modify listener from Properties General Tab Name Section

--- plugins/com.cisco.yangide.ext.model.editor/src/com/cisco/yangide/ext/model/editor/property/GeneralTabNameSection.java
+++ plugins/com.cisco.yangide.ext.model.editor/src/com/cisco/yangide/ext/model/editor/property/GeneralTabNameSection.java
@@ -1,12 +1,16 @@
 package com.cisco.yangide.ext.model.editor.property;
 
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.ChangeEvent;
+import org.eclipse.core.databinding.observable.IChangeListener;
+import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Composite;
@@ -16,14 +20,15 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
 
 import com.cisco.yangide.ext.model.NamedNode;
-import com.cisco.yangide.ext.model.editor.util.PropertyUtil;
 import com.cisco.yangide.ext.model.editor.util.YangModelUIUtil;
 import com.cisco.yangide.ext.model.editor.util.YangModelUtil;
 
 public class GeneralTabNameSection extends GFPropertySection implements ITabbedPropertyConstants {
 
     private Text nameText;
-    private NamedNode node; 
+    private NamedNode node;
+    private DataBindingContext bindingContext = new DataBindingContext();
+    private Binding binding;
     
     @Override
     public void createControls(Composite parent,
@@ -33,25 +38,13 @@ public class GeneralTabNameSection extends GFPropertySection implements ITabbedP
         TabbedPropertySheetWidgetFactory factory = getWidgetFactory();
         Composite composite = factory.createFlatFormComposite(parent);
         FormData data;
- 
+       
         nameText = factory.createText(composite, "");
         data = new FormData();
         data.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
-        data.right = new FormAttachment(100, 0);
+        data.right = new FormAttachment(50, 0);
         data.top = new FormAttachment(0, VSPACE);
         nameText.setLayoutData(data);
-        nameText.addModifyListener(new ModifyListener() {
-            
-            @Override
-            public void modifyText(ModifyEvent e) {
-                if (null != node) {
-                    node.setName(((Text) e.widget).getText());
-                    YangModelUIUtil.updatePictogramElement(getDiagramTypeProvider().getFeatureProvider(), 
-                            YangModelUIUtil.getBusinessObjectPropShape(getDiagramTypeProvider().getFeatureProvider(), node, PropertyUtil.OBJECT_HEADER_TEXT_SHAPE_KEY));
-                }
-                
-            }
-        });
  
         CLabel valueLabel = factory.createCLabel(composite, "Name:");
         data = new FormData();
@@ -63,6 +56,11 @@ public class GeneralTabNameSection extends GFPropertySection implements ITabbedP
  
     @Override
     public void refresh() {
+        if (null != binding) {
+            binding.updateTargetToModel();
+            binding.dispose();
+            bindingContext.removeBinding(binding);
+        }
         PictogramElement pe = getSelectedPictogramElement();
         if (pe != null) {
             Object bo = Graphiti.getLinkService()
@@ -71,7 +69,19 @@ public class GeneralTabNameSection extends GFPropertySection implements ITabbedP
                 return;
             }
             node = ((NamedNode) bo);
-            nameText.setText(node.getName() == null ? "" : node.getName());
+            binding = bindingContext.bindValue(
+                    WidgetProperties.text(SWT.Modify).observeDelayed(2000, nameText), EMFProperties
+                            .value(YangModelUtil.MODEL_PACKAGE.getNamedNode_Name()).observe(node));
+            binding.updateModelToTarget();
+            binding.getModel().addChangeListener(new IChangeListener() {
+                
+                @Override
+                public void handleChange(ChangeEvent event) {
+                    YangModelUIUtil
+                    .updatePictogramElement(getDiagramTypeProvider().getFeatureProvider(), YangModelUIUtil.getBusinessObjectPropShape(getDiagramTypeProvider().getFeatureProvider(),
+                            node, YangModelUtil.MODEL_PACKAGE.getNamedNode_Name()));
+                }
+            });
         }
     }
 }
