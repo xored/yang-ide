420cc13614ac38918cbcb275f30a176734758729
Konstantin Zaitsev <konstantin.zaitsev@xored.com>
2014-08-26 16:41:22 +0700

    * Fixed double click selection in the Yang Editor.

--- plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/YangDoubleClickStrategy.java
+++ plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/YangDoubleClickStrategy.java
@@ -10,29 +10,34 @@ package com.cisco.yangide.editor.editors;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 
 /**
  * @author Alexey Kholupko
  */
 public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
-    protected ITextViewer fText;
 
+    @Override
     public void doubleClicked(ITextViewer part) {
-        int pos = part.getSelectedRange().x;
-
-        if (pos < 0)
-            return;
-
-        fText = part;
-
-        if (!selectComment(pos)) {
-            selectWord(pos);
+        ISelectionProvider selectionProvider = part.getSelectionProvider();
+        if (selectionProvider != null) {
+            ISelection selection = selectionProvider.getSelection();
+            if (selection != null && selection instanceof ITextSelection) {
+                int pos = ((ITextSelection) selection).getOffset();
+                if (pos > 0) {
+                    if (!selectComment(part, pos)) {
+                        selectWord(part, pos);
+                    }
+                }
+            }
         }
     }
 
-    protected boolean selectComment(int caretPos) {
-        IDocument doc = fText.getDocument();
+    private boolean selectComment(ITextViewer part, int caretPos) {
+        IDocument doc = part.getDocument();
         int startPos, endPos;
 
         try {
@@ -45,13 +50,15 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
                     pos -= 2;
                     continue;
                 }
-                if (c == Character.LINE_SEPARATOR || c == '\"')
+                if (c == Character.LINE_SEPARATOR || c == '\"') {
                     break;
+                }
                 --pos;
             }
 
-            if (c != '\"')
+            if (c != '\"') {
                 return false;
+            }
 
             startPos = pos;
 
@@ -61,18 +68,20 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
 
             while (pos < length) {
                 c = doc.getChar(pos);
-                if (c == Character.LINE_SEPARATOR || c == '\"')
+                if (c == Character.LINE_SEPARATOR || c == '\"') {
                     break;
+                }
                 ++pos;
             }
-            if (c != '\"')
+            if (c != '\"') {
                 return false;
+            }
 
             endPos = pos;
 
             int offset = startPos + 1;
             int len = endPos - offset;
-            fText.setSelectedRange(offset, len);
+            part.setSelectedRange(offset, len);
             return true;
         } catch (BadLocationException x) {
         }
@@ -80,10 +89,10 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
         return false;
     }
 
-    protected boolean selectWord(int caretPos) {
+    protected boolean selectWord(ITextViewer part, int caretPos) {
 
-        IDocument doc = fText.getDocument();
-        int startPos, endPos;
+        IDocument doc = part.getDocument();
+        int startPos;
 
         try {
 
@@ -92,8 +101,9 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
 
             while (pos >= 0) {
                 c = doc.getChar(pos);
-                if (!Character.isJavaIdentifierPart(c))
+                if (!isYangIdentifierPart(c)) {
                     break;
+                }
                 --pos;
             }
 
@@ -104,13 +114,13 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
 
             while (pos < length) {
                 c = doc.getChar(pos);
-                if (!Character.isJavaIdentifierPart(c))
+                if (!isYangIdentifierPart(c)) {
                     break;
+                }
                 ++pos;
             }
 
-            endPos = pos;
-            selectRange(startPos, endPos);
+            part.setSelectedRange(startPos + 1, pos - startPos - 1);
             return true;
 
         } catch (BadLocationException x) {
@@ -119,9 +129,7 @@ public class YangDoubleClickStrategy implements ITextDoubleClickStrategy {
         return false;
     }
 
-    private void selectRange(int startPos, int stopPos) {
-        int offset = startPos + 1;
-        int length = stopPos - offset;
-        fText.setSelectedRange(offset, length);
+    private boolean isYangIdentifierPart(char c) {
+        return Character.isJavaIdentifierPart(c) || c == '-' || c == ':';
     }
 }
