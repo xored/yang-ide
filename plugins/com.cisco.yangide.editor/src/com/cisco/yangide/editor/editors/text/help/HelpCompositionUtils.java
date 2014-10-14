56542123230ad1cb2d1899dbbfbd3880f9efada6
Kirill Karmakulov <Kirill.Karmakulov@xored.com>
2014-10-14 10:12:02 +0700

    Copyrights fixed for new files

--- plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/text/help/HelpCompositionUtils.java
+++ plugins/com.cisco.yangide.editor/src/com/cisco/yangide/editor/editors/text/help/HelpCompositionUtils.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */ 
 package com.cisco.yangide.editor.editors.text.help;
 
 import org.eclipse.jdt.ui.PreferenceConstants;
@@ -146,7 +153,7 @@ public class HelpCompositionUtils {
     /**
      * Returns a stylesheet used among quick-help infopopups. 
      */
-    private static String getStyleSheet() {
+    public static String getStyleSheet() {
         if (YangTextHover.styleSheet == null) {
             YangTextHover.styleSheet = YangEditorPlugin.getDefault().getBundleFileContent(
                     "/resources/HoverStyleSheet.css");
